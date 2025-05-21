8ef1ad892dc4441e994c01f10c4c40283215654d
cl <351033166@qq.com>
2025-05-21 20:28:55 +0800

    设置启动图

--- test7/test7/AppDelegate.m
+++ test7/test7/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -15,12 +16,33 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-       self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor grayColor];
+    // 首先设置 window 和背景色
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor redColor];
+    // 设置初始根视图控制器
     UIViewController *vc = [[UIViewController alloc] init];
-       self.window.rootViewController = vc;
-       [self.window makeKeyAndVisible];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
+    
+    // 创建临时启动视图
+    UIView *launchView = [[UIView alloc] initWithFrame:self.window.bounds];
+    launchView.backgroundColor = [UIColor whiteColor];
+    UIImageView *logo = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"LaunchImage"]];
+    logo.center = launchView.center;
+    [launchView addSubview:logo];
+    
+    
+    
+    // 添加启动视图
+    [self.window addSubview:launchView];
+    
+    // 延迟移除启动图
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [launchView removeFromSuperview];
+        ViewController *mainVc = [[ViewController alloc] init];
+        self.window.rootViewController = mainVc;
+    });
+    
     return YES;
 }
 
--- test7/test7/ViewController.m
+++ test7/test7/ViewController.m
@@ -16,6 +16,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    UILabel *label = [[UILabel alloc] init];
+    label.frame =CGRectMake(100, 100, 200, 100);
+    label.text=@"sdsdsdsdsd";
+    [self.view addSubview:label];
 }
 
 
